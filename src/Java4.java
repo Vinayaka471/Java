396503bb8f646821c4aacc4c38e76ba94a6ce476
Vinayaka <vinayakah47@gmail.com>
2026-05-19 11:44:41 +0530

    update variable value

---
+++ src/Java4.java
@@ -0,0 +1,25 @@
+//Problem 3
+//
+//Update variable value.
+//
+//        Example:
+//
+//int x = 5;
+//
+//Change to:
+//
+//        10
+//
+//and print final value.
+
+
+
+public class Java4 {
+    public static void main(String[] args){
+        int x=5;
+        x=10;
+        System.out.println(x);
+
+        }
+    }
+
