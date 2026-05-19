75c382d37d1b26425154c10d2ebb367f784dd557
Vinayaka <vinayakah47@gmail.com>
2026-05-19 12:02:40 +0530

    Java Programs

---
+++ src/Java5.java
@@ -0,0 +1,19 @@
+//Problem 4
+//
+//Store:
+//
+//decimal number
+//character
+//boolean value
+//
+//Print them.
+
+
+public class Java5 {
+    public static void main(String[] args){
+        float goat=12.3f;
+        char tag='V';
+        boolean sunrise=true;
+        System.out.println("goat: " +goat+" "+ "tag: "+tag+" "+"sunrise: "+sunrise);
+    }
+}
