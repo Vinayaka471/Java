bdd230fd7e8a6ca31eaa7102e275c275da67051e
Vinayaka <vinayakah47@gmail.com>
2026-05-19 11:08:11 +0530

    Variables declaration, initialization and printed values

--- src/Java2.java
+++ src/Java2.java
@@ -1,11 +1,11 @@
 public class Java2 {
-    public static void main(String[] args){
-        String name="Vinayaka";
-        int age=23;
-        String college="PESITM Shivamogga";
-//        System.out.println("name: "+name +" "+ "age :"+age +" "+ "college: "+college);
-        System.out.println("name :"+name);
-        System.out.println("age :"+age);
-        System.out.println("college :"+college);
+    public static void main(String[] args) {
+        String name = "Vinayaka";
+        int age = 23;
+        String college = "PESITM Shivmogga";
+//        System.out.println("name: " + name + " " + "age :" + age + " " + "college: " + college + " ");
+        System.out.println("name: "+name);
+        System.out.println("age: "+age);
+        System.out.println("college: "+college);
     }
 }
