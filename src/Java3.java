21945027e77852db6aca241f0438527ed5c1b01f
Vinayaka <vinayakah47@gmail.com>
2026-05-19 11:23:33 +0530

    store int values and print the sum,subtractin and multiplication

---
+++ src/Java3.java
@@ -0,0 +1,25 @@
+//Problem 2
+//
+//Store two integers and print:
+//
+//sum
+//        subtraction
+//multiplication
+
+public class Java3 {
+    public static void main(String[] args){
+        int a=42,b=36;
+        int c=a+b;
+        System.out.println(c); //sum value
+
+        int l=12;
+        int s=8;
+        int t=l-s;
+        System.out.println(t); //subtraction value
+
+        int v=46;
+        int y=13;
+        int z=v*y;
+        System.out.println(z); //multiplication value
+    }
+}
