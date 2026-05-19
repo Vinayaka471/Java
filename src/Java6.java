b0a2e8a03d853adf9a0146993f53e8810d64d0fa
Vinayaka <vinayakah47@gmail.com>
2026-05-19 15:07:54 +0530

    Adding two numbers from using users input

--- src/Java6.java
+++ src/Java6.java
@@ -1,2 +1,23 @@
-public class Java6 {
-}
+    //Problem 5
+    //
+    //Take two numbers from user and print:
+    //
+    //addition
+    //        subtraction
+    //multiplication
+    //        division
+    //remainder
+
+    import java.util.Scanner;
+
+    public class Java6 {
+        public static void main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the number of carrot: ");
+            int carrot=sc.nextInt();
+            System.out.println("Enter the number of onion: ");
+            int onion=sc.nextInt();
+            System.out.println("Total vegetable is :" + (carrot+onion));
+
+        }
+    }
