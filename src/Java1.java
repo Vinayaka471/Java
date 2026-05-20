1cf9ec19dd284ce98f19dc76966d61ed0dc6bfe6
Vinayaka <vinayakah47@gmail.com>
2026-05-20 19:25:47 +0530

    Antigravity 2.0 Released on 19th/20th may 2026

--- src/Java1.java
+++ src/Java1.java
@@ -1,5 +1,6 @@
 public class Java1 {
     public static void main(String[] args){
         System.out.println("Vinayaka");
+        System.out.println("Antigravity 2.0 without code editor on 19th/20th may 2026");
     }
 }
--- src/Java6.java
+++ src/Java6.java
@@ -12,12 +12,19 @@
 
     public class Java6 {
         public static void main(String[] args){
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter the number of carrot: ");
-            int carrot=sc.nextInt();
-            System.out.println("Enter the number of onion: ");
-            int onion=sc.nextInt();
-            System.out.println("Total vegetable is :" + (carrot+onion));
+//            Scanner sc=new Scanner(System.in);
+//            System.out.println("Enter the number of carrot: ");
+//            int carrot=sc.nextInt();
+//            System.out.println("Enter the number of onion: ");
+//            int onion=sc.nextInt();
+//            System.out.println("Total vegetable is :" + (carrot+onion));
+
+//                Scanner sc=new Scanner(System.in);
+//                System.out.println("Enter the total amount: ");
+//                Scanner total=sc.nextInt();
+//                System.out.println("Enter the expenditure: ");
+//                Scanner expenditure=sc.nextInt();
+//                System.out.println("Remaining money is: "+(total-expenditure));
 
         }
     }
